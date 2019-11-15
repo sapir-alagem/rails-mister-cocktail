e21f009bee75459300981d61afbb6a4fc9c052ef
sapir alagem <sapir.alagem@gmail.com>
2019-11-15 19:41:21 +0000

    seed for ingerdient and cockatils

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,7 +1,7 @@
 class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
-  end
+end
 
   def show
     set_cocktail
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,12 @@ json["drinks"].each do |d|
   Ingredient.create!(name: d["strIngredient1"])
 end
 puts "100 ingredients was created"
+
+puts "creating 6 cocktails"
+Cocktail.create!(name: 'Negroni', remote_photo_url: 'https://www.thedrinkblog.com/wp-content/uploads/2015/11/tdb-negroni-1500x1000-3-1.jpg')
+Cocktail.create!(name: 'Grog', remote_photo_url: 'https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+Cocktail.create!(name: 'Gin&Tonic', remote_photo_url: 'https://images.unsplash.com/photo-1504675099198-7023dd85f5a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80')
+Cocktail.create!(name: 'Bellini', remote_photo_url: 'https://images.unsplash.com/photo-1562001434-4697c7ad23ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80')
+Cocktail.create!(name: 'Horchata', remote_photo_url: 'https://www.thedrinkblog.com/wp-content/uploads/2018/06/tdb-horchata-1500x1000.jpg')
+Cocktail.create!(name: 'Apricot whisky sour', remote_photo_url: 'https://www.thedrinkblog.com/wp-content/uploads/2018/06/tdb-apricot-whisky-1500x1000.jpg')
+puts "done"
